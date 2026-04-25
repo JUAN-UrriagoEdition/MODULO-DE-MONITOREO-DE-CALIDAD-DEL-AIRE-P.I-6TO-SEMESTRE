a25076162beeae5d1c36b91de4a4ed7659f60932
Miguel Angel Parra Gutierrez <miguel.parra03@uceva.edu.co>
2026-04-24 20:21:00 -0500

    Descarga Automatica del repositorio para colaboradores

---
+++ Descargar Repo.bat
@@ -0,0 +1,6 @@
+@echo off
+cd /d "%~dp0"
+git clone https://github.com/JUAN-UrriagoEdition/MODULO-DE-MONITOREO-DE-CALIDAD-DEL-AIRE-P.I-6TO-SEMESTRE.git
+echo.
+echo Listo! Repositorio clonado correctamente.
+pause
